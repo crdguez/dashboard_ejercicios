9d6f0e54a764d8a4bd934f50323d3a41fe6045bb
crdguez <carlos.rodriguez.jaso@gmail.com>
2023-05-24 15:15:49 +0200

    z

--- main.py
+++ main.py
@@ -2,7 +2,14 @@ import streamlit as st
 import base64
 import os
 
-    
+st.set_page_config(
+    page_title='Solucionario',
+    page_icon="🧊",
+    layout='wide')
+st.sidebar.title('Solucionario')
+
+st.sidebar.write([filename for filename in os.listdir(file_to_search) if os.path.isdir(os.path.join(file_to_search,filename))])
+
 def show_pdf(file_path):
     with open(file_path,"rb") as f:
         base64_pdf = base64.b64encode(f.read()).decode('utf-8')
@@ -21,11 +28,7 @@ st.write(files)
 for f in files :
     st.header(f.split(".pdf")[0])
     show_pdf(f)
-    st.download_button(label="Descargar fichero", 
+    st.download_button(label="Descargar fichero",
         data=open(f, "rb").read(),
         file_name=f,
         mime='application/octet-stream')
-
-
-
-
