26126fe23827b2f7d82b3d1ce8eb3c2e027a2bb5
crdguez <carlos.rodriguez.jaso@gmail.com>
2023-05-24 15:48:16 +0200

    z

--- main.py
+++ main.py
@@ -2,6 +2,13 @@ import streamlit as st
 import base64
 import os
 
+def show_pdf(file_path):
+    with open(file_path,"rb") as f:
+        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
+#     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
+    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+    st.markdown(pdf_display, unsafe_allow_html=True)
+
 st.set_page_config(
     page_title='Solucionario',
     page_icon="🧊",
@@ -20,13 +27,13 @@ curso = st.sidebar.selectbox('Curso:',lc,0)
 lb=[b for b in next(os.walk(curso))[1]]
 bloque = st.sidebar.selectbox('Bloque:',lb,0)
 
-def show_pdf(file_path):
-    with open(file_path,"rb") as f:
-        base64_pdf = base64.b64encode(f.read()).decode('utf-8')
-#     pdf_display = f'<iframe src="data:application/pdf;base64,{base64_pdf}" width="800" height="800" type="application/pdf"></iframe>'
-    pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
-    st.markdown(pdf_display, unsafe_allow_html=True)
 
+ruta=curso+r'/'+bloque
+
+# Filtro fichero
+
+lf = [f for f in os.listdir(ruta) if f.endswith('.pdf')]
+fichero = st.sidebar.selectbox('Bloque:',lb,0)
 
 
 st.title('Ejercicios de Matemáticas')
@@ -35,14 +42,12 @@ st.title('Ejercicios de Matemáticas')
 # files = [f for f in os.listdir('.') if f.endswith('.pdf')]
 # st.write(files)
 
-ruta=curso+r'/'+bloque
-files = [f for f in os.listdir(ruta) if f.endswith('.pdf')]
-st.write(files)
 
-for f in files :
-    st.header(f.split(".pdf")[0])
-    show_pdf(ruta+"/"+f)
-    st.download_button(label="Descargar fichero",
-        data=open(ruta+"/"+f, "rb").read(),
-        file_name=f,
-        mime='application/octet-stream')
+st.write(lb)
+
+st.header(fichero.split(".pdf")[0])
+show_pdf(ruta+"/"+fichero)
+st.download_button(label="Descargar fichero",
+    data=open(ruta+"/"+fichero, "rb").read(),
+    file_name=f,
+    mime='application/octet-stream')
